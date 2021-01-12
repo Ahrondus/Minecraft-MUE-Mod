3afaa0e1b0495b7c70b332a31586944834d72872
Ahrondus <62456961+Ahrondus@users.noreply.github.com>
2021-01-11 21:59:35 -0600

    Switched to Deferred Registry
    Updated Item initialization to Deferred Registry, optimizing code.

--- src/main/java/com/ahrondus/moreusefuleggs/MoreUsefulEggs.java
+++ src/main/java/com/ahrondus/moreusefuleggs/MoreUsefulEggs.java
@@ -2,14 +2,13 @@ package com.ahrondus.moreusefuleggs;
 
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.ahrondus.moreusefuleggs.init.ItemInitDR;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("moreusefuleggs")
@@ -27,7 +26,8 @@ public class MoreUsefulEggs
     	
         // Register the setup method for mod loading
         modEventBus.addListener(this::setup);
-        modEventBus.addListener(this::doClientStuff);
+        
+        ItemInitDR.ITEMS.register(modEventBus);
         
         instance = this;
 
@@ -36,17 +36,6 @@ public class MoreUsefulEggs
     }
 
     private void setup(final FMLCommonSetupEvent event)
-    {
-    	
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) 
-    {
-    	
-    }
-    
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) 
     {
     	
     }
